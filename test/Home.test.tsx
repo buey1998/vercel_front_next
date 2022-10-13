c74c9e79443321723299e9ea41933b52dd050db9
mkan <m@kos.co.th>
2022-10-13 17:17:09 +0700

    fix : 🧪 create test  folder

--- test/Home.test.tsx
+++ test/Home.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 
 import '@testing-library/jest-dom';
 
-import Home from '../index';
+import Home from '../src/pages/index';
 
 it('Should render title text', () => {
   render(<Home />);
